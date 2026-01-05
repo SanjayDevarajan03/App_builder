e6c7131deacab21887609c8cfedb6690c6d6e01c
sanjay030102 <sdevara6@ncsu.edu>
2026-01-04 23:41:41 -0500

    Completed README.md

--- agent/graph.py
+++ agent/graph.py
@@ -1,54 +1,56 @@
-from langchain_groq import ChatGroq 
 from dotenv import load_dotenv
 from langchain.globals import set_verbose, set_debug
+from langchain_groq.chat_models import ChatGroq
 from langgraph.constants import END
 from langgraph.graph import StateGraph
 from langgraph.prebuilt import create_react_agent
-from pydantic import BaseModel
+
 from prompts import *
 from states import *
 from tools import write_file, read_file, get_current_directory, list_files
 
+_ = load_dotenv()
+
 set_debug(True)
 set_verbose(True)
-_=load_dotenv()
+
 llm = ChatGroq(model="openai/gpt-oss-120b")
 
+
 def planner_agent(state: dict) -> dict:
-    """Converts user prompt into a structured plan."""
+    """Converts user prompt into a structured Plan."""
     user_prompt = state["user_prompt"]
     resp = llm.with_structured_output(Plan).invoke(
         planner_prompt(user_prompt)
     )
     if resp is None:
         raise ValueError("Planner did not return a valid response.")
-    return {"plan":resp}
+    return {"plan": resp}
+
 
 def architect_agent(state: dict) -> dict:
     """Creates TaskPlan from Plan."""
     plan: Plan = state["plan"]
     resp = llm.with_structured_output(TaskPlan).invoke(
-        architect_prompt(plan.model_dump_json())
+        architect_prompt(plan=plan.model_dump_json())
     )
     if resp is None:
         raise ValueError("Planner did not return a valid response.")
+
     resp.plan = plan
     print(resp.model_dump_json())
     return {"task_plan": resp}
 
-#resp = llm.with_structured_output(Schema).invoke("Extract Price and EPS from this report: NVIDIA reported quarterly EPS of 2.3 and thier current price is $100.")
-
-#print(resp.price)
 
 def coder_agent(state: dict) -> dict:
-    """Langgraph tool-using coder agent."""
+    """LangGraph tool-using coder agent."""
     coder_state: CoderState = state.get("coder_state")
     if coder_state is None:
         coder_state = CoderState(task_plan=state["task_plan"], current_step_idx=0)
-    
+
     steps = coder_state.task_plan.implementation_steps
     if coder_state.current_step_idx >= len(steps):
-        return {"coder_state":coder_state, "status": "DONE"}
+        return {"coder_state": coder_state, "status": "DONE"}
 
     current_task = steps[coder_state.current_step_idx]
     existing_content = read_file.run(current_task.filepath)
@@ -64,26 +66,30 @@ def coder_agent(state: dict) -> dict:
     coder_tools = [read_file, write_file, list_files, get_current_directory]
     react_agent = create_react_agent(llm, coder_tools)
 
-    react_agent.invoke({"messages":[{"role":"system","content":system_prompt},
-                                    {"role":"user", "content":user_prompt}]})
+    react_agent.invoke({"messages": [{"role": "system", "content": system_prompt},
+                                     {"role": "user", "content": user_prompt}]})
 
     coder_state.current_step_idx += 1
-    return {"coder_state":coder_state}
+    return {"coder_state": coder_state}
 
 
-builder = StateGraph(dict)
-builder.add_node("planner", planner_agent)
-builder.add_node("architect", architect_agent)
-builder.add_node("coder", coder_agent)
+graph = StateGraph(dict)
 
-builder.add_edge("planner", "architect")
-builder.add_edge("architect", "coder")
-builder.add_conditional_edges(
-        "coder", 
-        lambda s: "END" if s.get("status") == "DONE" else "coder",
-        {"END": END, "coder": "coder"}
-    )
-builder.set_entry_point("planner")
-agent = builder.compile()
+graph.add_node("planner", planner_agent)
+graph.add_node("architect", architect_agent)
+graph.add_node("coder", coder_agent)
+
+graph.add_edge("planner", "architect")
+graph.add_edge("architect", "coder")
+graph.add_conditional_edges(
+    "coder",
+    lambda s: "END" if s.get("status") == "DONE" else "coder",
+    {"END": END, "coder": "coder"}
+)
+
+graph.set_entry_point("planner")
+agent = graph.compile()
 if __name__ == "__main__":
-    result = agent.invoke({"user_prompt":"Build a colourful modern todo app in that html, css, and js"})
+    result = agent.invoke({"user_prompt": "Build a colourful modern todo app in html css and js"},
+                          {"recursion_limit": 100})
+    print("Final State:", result)
